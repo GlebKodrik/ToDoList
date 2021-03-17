58b8176acbaa6773bef493358ff5ba3bb5e95ea3
BreDi <40304887+Glendi123@users.noreply.github.com>
2021-03-17 15:56:10 +0300

    compliet
    перечеркивание выполненной задачи

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import './App.css';
-import React, {Component} from 'react'
+import React, {useState} from 'react'
 import {TodoList} from "./TodoList";
+import {Button} from '@material-ui/core';
+import s from './TodoList.moduls.css'
 
-class App extends Component {
-    state = {
-        todos: [
-            {id: 1, title: "Выполнить первую задачу", completed: false},
-            {id: 2, title: "Погладить васю", completed: false},
-            {id: 3, title: "Рассказать историю", completed: false},
-            {id: 4, title: "Разгрузить коробки", completed: false},
-        ]
-    }
+const App = () => {
+    const [todos, setTodoList] = useState([]);
+    const [todoTitle, setTodoTitle] = useState("");
+    const [error, setError] = useState("");
+    const addTodo = () => {
+        if(todoTitle !== ''){
+            setError("");
+            setTodoList([
+                ...todos, {
+                    id: Date.now(),
+                    title: todoTitle,
+                    completed: false
+                }
+            ]);
+            setTodoTitle("");
+        }else {
+            setError("Пустое поле");
+        }
+    };
 
-    render() {
-        return (
-            <div className="container">
-                <h1>ToDo List</h1>
-                <div className="input-field">
-                    <input type="text"/>
-                    <label>Todo name</label>
-                </div>
-                <TodoList todos={this.state.todos}/>
+    return (
+        <div className="container">
+            <h1>ToDo List</h1>
+            <div className="input-field">
+                <input type="text"
+                       value={todoTitle}
+                       onChange={event => setTodoTitle(event.target.value)}
+                />
+                <label>Задача</label>
+                <Button onClick={addTodo} variant="contained" color="primary">
+                    Primary
+                </Button>
+                {error && <div className="error">{error}</div>}
             </div>
-        )
-    }
+            <TodoList todos={todos}/>
+        </div>
+    )
 }
 
 export default App;
--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const TodoListItem = (props) => {
+    const [checked , setChecked] = useState(props.completed);
+    const cln = ['todo'];
+    if(checked){
+        cln.push('completed');
+    }
     return (
-        <li className="todo">
+        <li className={cln.join(' ')}>
             <label>
-                <input type="checkbox" defaultChecked={false}/>
+                <input type="checkbox"
+                       checked={checked}
+                       onChange={() => setChecked(!checked)}
+                />
                 <span>{props.title}</span>
                 <i className="material-icons red-text">delete_forever</i>
             </label>
